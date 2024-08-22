f32f3d89247ab2d0360d82b1f1d5fd6a90839c48
Kongkon Jowarder <kongkon4545@gmail.com>
2024-08-22 22:56:24 +0600

    endpoint change

--- src/redux/features/api/animalApi.js
+++ src/redux/features/api/animalApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const animalApi = createApi({
     reducerPath : "animalApi",
-    baseQuery : fetchBaseQuery({baseUrl : "http://localhost:5000/api"}),
+    baseQuery : fetchBaseQuery({baseUrl : "https://antopolis-server-job-task.vercel.app/api"}),
     endpoints : (builder)=>({
         getAllAnimals : builder.query({
             query:()=> "/animal"
